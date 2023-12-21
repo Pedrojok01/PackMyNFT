fcbbfc81a7646fb7eef7f9b134ecb4f6c057b8fd
Pedrojok01 <pedrojok@pm.me>
2023-12-21 10:25:07 +0700

    Cleaning and reorg after merge to monorepo

--- URI_Generation/data.js
+++
@@ -1,7 +0,0 @@
-export const IPFS_img = "QmPnrvP1bWW3MH9YtJyUULq6N15KtYhRrvDuTcURUQJxSS";
-
-export const metadata = {
-  name: "Pack My NFT",
-  description: "Bundle your assets into a single NFT pack!",
-  generation: "1",
-};
